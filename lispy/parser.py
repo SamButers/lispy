fe5655a6364cbda4258b91a3a538af3d4d1e2eda
Sam Buters <sambuters@gmail.com>
2019-10-28 14:02:52 -0300

    Adding grammar updates and transformer

--- lispy/parser.py
+++ lispy/parser.py
@@ -5,8 +5,42 @@ from .runtime import Symbol
 
 
 class LispTransformer(InlineTransformer):
-    def start(self, *args): 
-        return [Symbol.BEGIN, *args]
+	def start(self, *args):
+		if(len(args) == 1):
+			if(isinstance(args[0], (float, int))):
+				return args[0]
+		return [Symbol.BEGIN, *args]
+		
+	def value(self, *args):
+		return list(args)
+		
+	def conditional(self, test, conseq, alt):
+		return [test,  conseq, alt]
+		
+	def test(self, comparison, atom1, atom2):
+		return [comparison, atom1, atom2]
+		
+	def definition(self, asymbol, exp):
+		return ['define', asymbol, exp]
+		
+	def procedure(self, proc, *args):
+		if(str(proc) in ('+' '-', '/', '*'):
+			return [proc, args[0], args[1]]
+		return [proc, list(args)]
+		
+	def atom(self, token):
+		try:
+			return int(token)
+		except ValueError:
+			try:
+				return float(token)
+			except ValueError:
+				if(str(token) == '#t'):
+					return True
+				if(str(token) == '#f'):
+					return False
+				
+				return Symbol(token)
 
 def parse(src: str):
     """
